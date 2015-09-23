b49c5940021f95ad23c58c02a32cd85832814596
DuskyShelf <dupsdale@gmail.com>
2015-09-23 11:09:56 +0100

    passed initial test

--- test/e2e/ShopFeatureSpec.js
+++ test/e2e/ShopFeatureSpec.js
@@ -3,8 +3,11 @@ describe("Setting a target", function() {
         browser.get("http://localhost:8080");
     });
 
-    it('should load the correct page', function () {
+    it('should load the correct homepage', function () {
       expect(browser.getTitle()).toEqual('So I Heard You Like Clothing');
     });
+
+    
+
 
 });
