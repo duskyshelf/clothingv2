e592c0eeb367cb36461edc9acfdb08655cf51d69
DuskyShelf <dupsdale@gmail.com>
2015-09-25 17:18:47 +0100

    building product factory spec file

--- test/e2e/ShopFeatureSpec.js
+++ test/e2e/ShopFeatureSpec.js
@@ -4,8 +4,17 @@ describe("Setting a target", function() {
         browser.get("http://localhost:8080");
     });
 
+    var productList = element(by.className("product-list")),
+        shoppingCart  = element(by.className("shopping-cart"));
+
     it('should load the correct homepage', function () {
       expect(browser.getTitle()).toEqual('So I Heard You Like Clothing');
+    });
+
+    describe('products', function () {
+      it('are loaded correctly onto the homepage', function () {
+        expect(productList.getText()).toContain('Almond Toe Court Shoes');
+      });
     });
 
 });
---
+++ test/unit/productfactory.spec.js
@@ -0,0 +1,14 @@
+describe('factory: Products', function() {
+
+  var products = [{ name: "Almond Toe Court Shoes, Patent Black",
+                    category: "Women's Footwear",
+                    price: 99.00,
+                    stock: 5 },
+                  { name: "Suede Shoes, Blue",
+                    category: "Women's Footwear",
+                    price: 42.00,
+                    stock: 4 }];
+
+
+
+});
