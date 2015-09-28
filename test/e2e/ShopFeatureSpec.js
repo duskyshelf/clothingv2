6a36d2eabb84e84f33018bb4064735b7b49f84c1
DuskyShelf <dupsdale@gmail.com>
2015-09-28 18:16:28 +0100

    added product item ids on button click

--- test/e2e/ShopFeatureSpec.js
+++ test/e2e/ShopFeatureSpec.js
@@ -5,16 +5,26 @@ describe("Setting a target", function() {
     });
 
     var productList = element(by.className("product-list")),
-        shoppingCart  = element(by.className("shopping-cart"));
+        shoppingCart  = element(by.className("shopping-cart")),
+        shopItemOne = element(by.id('item-0'));
 
     it('should load the correct homepage', function () {
       expect(browser.getTitle()).toEqual('So I Heard You Like Clothing');
     });
 
     describe('products', function () {
-      it('are loaded correctly onto the homepage', function () {
+      it('are loading correctly onto the homepage', function () {
         expect(productList.getText()).toContain('Almond Toe Court Shoes');
       });
     });
+
+    describe('shopping cart', function () {
+      it('can have items added to it', function() {
+        shopItemOne.click();
+        expect(shoppingCart.getText()).toContain('Almond Toe Court Shoes');
+      });
+    });
+
+
 
 });
