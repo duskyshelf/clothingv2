edb7bf97fe3d4d74e15d05c11bb49dc2920d0dd9
DuskyShelf <dupsdale@gmail.com>
2015-09-28 19:25:25 +0100

    added quantity ordered parameter to shopping basket items

--- public/js/shopFrontController.js
+++ public/js/shopFrontController.js
@@ -13,8 +13,28 @@ clothingShopFront.controller('ClothingShopController', [ 'productData', function
   self.basket = [];
 
   self.addItem = function(product) {
-    self.basket.push(product);
-  };
+    var found = self.basket.some(function(item) {
+      return item.name === product.name;
+    });
 
+    if (!found)
+      {
+        product.quantity = 1;
+        self.basket.push(product);
+        console.log(product);
+      }
+    else
+      {
+        for (var i in self.basket)
+          {
+            if (self.basket[i].name == product.name)
+              {
+                console.log(1);
+                console.log(product);
+                self.basket[i].quantity++;
+              }
+          }
+      }
+  };
 
 }]);
